1217c6c3737ca0c3fce624d45a2c33bc8ef43bbc
avneet singh <avneet_cochhar@hotmail.com>
2019-03-02 20:26:56 -0600

    modified gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,17 @@
 'use strict';
-
 //  Load plugins
-
 var gulp = require('gulp'), 
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),    
     runSequence = require('run-sequence'),
-    connect = require('gulp-connect'),
     del = require('del'),
-    size = require('gulp-size'),
-    gulpif = require('gulp-if'),
-    argv = require('yargs').argv,
     csso = require('gulp-csso'),
-    sass = require('gulp-sass'),
-    sassVariables = require('gulp-sass-variables') ;
-
-
-//Handle possible memory leaks in javascript, probably irrelevant now
-
-var events = require('events').EventEmitter.prototype._maxListeners = 100;
-
-// Check for --dev flag
-
-var isDev = !!(argv.dev);
+    sass = require('gulp-sass');
 
 //Paths to source files
-
 var paths = {
     //Consumer and global assets
-
     html: './src',
     consumerScss: './src/assets/scss',
     fonts: './src/assets/fonts',
@@ -37,118 +19,81 @@ var paths = {
     sounds: './src/assets/sound',
     scripts: './src/assets/js/',
    
-    //Output Variables
-    
-    dest: './views',
+    //Output Variables    
+    destHtml: './views',
+    dest: './public',
     destConsumerCSSLocal: './public/stylesheets',
     destJavaScript: './public/javascripts',
     destImages: './public/images',
-    destSounds: './public/sounds',
-    destFonts: './public/fonts'
+    destSounds: './public/sounds'
 };
 
 //Compile and minify scss => css
-
 gulp.task('consumerstyles', function () {
-
     return gulp.src([
                     paths.consumerScss + '/*.scss',
                     paths.consumerScss + '/!_*.scss'
-                ])
-        .pipe(gulpif(!isDev,
-                    sassVariables({
-                        $env: 'production'
-                    }),
-                    sassVariables({
-                        $env: 'development'
-                    })
-        ))
-        .pipe(gulpif(!isDev,
-                    sass({
-                        outputStyle: "nested"
-                    }).on('error', sass.logError),
-                    sass({
-                        outputStyle: 'expanded'
-                    }).on('error', sass.logError)
-        ))
-        .pipe(gulpif(!isDev, csso()))
-        .pipe(gulpif(!isDev, 
-             gulp.dest(paths.destConsumerCSSLocal)))
-        .pipe(size({
-            title: 'Consumer CSS'
-        }))
-        .pipe(connect.reload())
+                ])       
+        .pipe(sass({
+                    outputStyle: 'expanded'
+                }).on('error', sass.logError)
+        )
+        .pipe(csso())
+        .pipe(gulp.dest(paths.destConsumerCSSLocal));
+});
+
+//Run all SCSS => CSS tasks
+gulp.task('styles', function () {
+    runSequence('consumerstyles');
 });
 
 //Concat and minify javascript files
-
 gulp.task('mainJS', function () {
     return gulp.src([
                     paths.scripts + 'blastradius/*.js'
                 ])
         .pipe(concat('scripts.js'))
-        .pipe(gulpif(!isDev, rename({
+        .pipe(rename({
             suffix: '.min'
-        })))
-        .pipe(gulpif(!isDev, uglify()))
-        .pipe(gulp.dest(paths.destJavaScript))
-        .pipe(size({
-            title: 'Main JS'
         }))
-        .pipe(connect.reload());
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destJavaScript));
 });
 
 gulp.task('vendors', function () {
     return gulp.src([paths.scripts + 'vendor/*.js'
                 ])
-        .pipe(gulpif(!isDev, rename({
+        .pipe(rename({
             suffix: '.min'
-        })))
-        .pipe(gulpif(!isDev, uglify()))
-        .pipe(gulp.dest(paths.destJavaScript))
-        .pipe(size({
-            title: 'Vendor JS'
         }))
-        .pipe(connect.reload());
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destJavaScript));
 });
 
 //Run all script tasks
-
 gulp.task('scripts', function () {
     runSequence(
         ['mainJS'], 
         'vendors');
 });
 
-//Run all SCSS => CSS tasks
-
-gulp.task('styles', function () {
-    runSequence('consumerstyles');
-});
-
 //Copy images for local work, could do some image optimization here in the future
-
 gulp.task('images', function () {
     return gulp.src(paths.images + '/**/*.{png,jpg,jpeg,gif,svg,ico}')
-        .pipe(gulp.dest(paths.destImages))
-        .pipe(connect.reload());
+        .pipe(gulp.dest(paths.destImages));
 });
 gulp.task('sounds', function () {
     return gulp.src(paths.sounds + '/**/*.wav')
-        .pipe(gulp.dest(paths.destSounds))
-        .pipe(connect.reload());
+        .pipe(gulp.dest(paths.destSounds));
 });
 
 //Compile HTML templates
-
 gulp.task('compileHTML', function () {
     gulp.src(paths.html + '/**/*.hbs')
-        .pipe(gulp.dest(paths.dest))
-        .pipe(connect.reload());
+        .pipe(gulp.dest(paths.destHtml));
 });
 
 //Watch task
-
 gulp.task('watch', function () {
     //livereload.listen(); - uncomment to enable live reload
     gulp.watch(paths.consumerScss + '/**/*.scss', ['styles']);
@@ -159,10 +104,9 @@ gulp.task('watch', function () {
 });
 
 //Clean dist directory
-
 gulp.task('clean', function () {
     return del([
-        paths.dest + '/**/*.*'
+        paths.destHtml,paths.dest
     ], {
         force: true
     }).then(paths => {
@@ -171,10 +115,9 @@ gulp.task('clean', function () {
 });
 
 //Default task
-
 gulp.task('default', ['clean'], function () {
     runSequence(
         ['images', 'scripts', 'styles', 'compileHTML','sounds'],
-         'watch'
+     'watch'
     );    
 });
--- refresh.bat
+++ refresh.bat
@@ -5,5 +5,3 @@ docker rm -v game
 docker build -t avneet7193/game .
 
 docker run -p 8080:3000 -d --name game avneet7193/game
-
-docker exec -it game sh -c gulp
